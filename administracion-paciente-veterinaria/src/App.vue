6525e2aed54963b468ea8a59b3183fce6a203ffa
EliasCode79 <eliasfranco2379@gmail.com>
2023-10-29 21:34:45 -0400

    feat: Se agrego multiples v-models con una sola variables reactiva, y el componente q lo recibe (formulario.vue) Se agrego los props, emits y :value para q se comuniquen entre componentes padre y hijo

--- administracion-paciente-veterinaria/src/App.vue
+++ administracion-paciente-veterinaria/src/App.vue
@@ -1,7 +1,15 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
 const pacientes = ref([]);
+
+const paciente = reactive({
+	nombre: 'este es nombre',
+	propietario: '',
+	email: '',
+	alta: '',
+	sintomas: '',
+});
 
 import Header from './components/Header.vue';
 import Formulario from './components/Formulario.vue';
@@ -10,7 +18,14 @@ import Formulario from './components/Formulario.vue';
 	<div class="container mx-auto mt-20">
 		<Header />
 		<div class="mt-12 md:flex mx-auto">
-			<Formulario />
+			<!-- Formulario con multiples v-models -->
+			<Formulario
+				v-model:nombre="paciente.nombre"
+				v-model:propietario="paciente.propietario"
+				v-model:email="paciente.email"
+				v-model:alta="paciente.alta"
+				v-model:sintomas="paciente.sintomas"
+			/>
 			<div class="md:w-1/2 md:h-screen overflow-y-scroll">
 				<h3 class="font-black text-3xl text-center">Administra tus pacientes</h3>
 				<div v-if="pacientes.length > 0" class=""></div>
--- administracion-paciente-veterinaria/src/components/Formulario.vue
+++ administracion-paciente-veterinaria/src/components/Formulario.vue
@@ -2,21 +2,38 @@
 import { reactive } from 'vue';
 import Alerta from './Alerta.vue';
 
+const props = defineProps({
+	nombre: {
+		type: String,
+		required: true,
+	},
+	propietario: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	alta: {
+		type: String,
+		required: true,
+	},
+	sintomas: {
+		type: String,
+		required: true,
+	},
+});
+
+defineEmits(['update:nombre', 'update:propietario', 'update:email', 'update:alta', 'update:sintomas']);
+
 const alerta = reactive({
 	mensaje: '',
 	tipo: '',
 });
 
-const paciente = reactive({
-	nombre: '',
-	propietario: '',
-	email: '',
-	alta: '',
-	sintomas: '',
-});
-
 const validar = () => {
-	if (Object.values(paciente).includes('')) {
+	if (Object.values(props).includes('')) {
 		alerta.mensaje = 'Todos los campos son obligatorios';
 		alerta.tipo = 'error';
 		return;
@@ -38,12 +55,14 @@ const validar = () => {
 		<form class="bg-white shadow-md rounded-lg py-10 px-5 mb-10" @submit.prevent="validar">
 			<div class="mb-5">
 				<label for="mascota" class="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
+				<!-- el update:propietario es un emit y podemos el :value(linea 75) para q cuando se inicie con un valor se llegue a mostrar -->
 				<input
 					class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
 					id="mascota"
 					type="text"
 					placeholder="Nombre de la mascota"
-					v-model="paciente.nombre"
+					:value="nombre"
+					@input="$emit('update:nombre', $event.target.value)"
 				/>
 			</div>
 			<div class="mb-5">
@@ -53,7 +72,8 @@ const validar = () => {
 					id="propietario"
 					type="text"
 					placeholder="Nombre del Propietario"
-					v-model="paciente.propietario"
+					:value="propietario"
+					@input="$emit('update:propietario', $event.target.value)"
 				/>
 			</div>
 			<div class="mb-5">
@@ -63,26 +83,29 @@ const validar = () => {
 					id="email"
 					type="email"
 					placeholder="Email"
-					v-model="paciente.email"
+					:value="email"
+					@input="$emit('update:email', $event.target.value)"
 				/>
 			</div>
 			<div class="mb-5">
 				<label for="alta" class="block text-gray-700 uppercase font-bold">Alta</label>
 				<input
-					v-model="paciente.alta"
 					class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
 					id="alta"
 					type="date"
+					:value="alta"
+					@input="$emit('update:alta', $event.target.value)"
 				/>
 			</div>
 			<div class="mb-5">
 				<label for="sintomas" class="block text-gray-700 uppercase font-bold">Sintomas</label>
 				<textarea
-					v-model="paciente.sintomas"
 					class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
 					id="sintomas"
 					type="text"
 					placeholder="Describe los sintomas"
+					:value="sintomas"
+					@input="$emit('update:sintomas', $event.target.value)"
 				/>
 			</div>
 
