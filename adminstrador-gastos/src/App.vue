07df542e5106be7050e157e9a19907e90410d070
EliasCode79 <eliasfranco2379@gmail.com>
2023-10-30 23:53:56 -0400

    init: Se inicio con el proyecto de adminstracion de gastos

---
+++ adminstrador-gastos/src/App.vue
@@ -0,0 +1,53 @@
+<script setup></script>
+
+<template>
+	<div>
+		<header>
+			<h1>Planificardor de gastos</h1>
+		</header>
+	</div>
+</template>
+
+<style>
+:root {
+	--azul: #3b82f6;
+	--blanco: #fff;
+	--gris-claro: #f5f5f5;
+	--gris: #94a3b8;
+	--gris-oscuro: #64748b;
+	--negro: #000;
+}
+
+html {
+	box-sizing: border-box;
+	font-size: 62.5%;
+}
+
+*,
+*:before,
+*:after {
+	box-sizing: inherit;
+}
+
+body {
+	font-size: 1.6rem;
+	font-family: 'Lato', sans-serif;
+	background-color: var(--gris-claro);
+}
+
+h1 {
+	font-size: 4rem;
+}
+h2 {
+	font-size: 3rem;
+}
+header {
+	background-color: var(--azul);
+}
+header h1 {
+	padding: 3rem 0;
+	margin: 0;
+	color: var(--blanco);
+	text-align: center;
+}
+</style>
